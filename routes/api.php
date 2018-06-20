b35139a58864de30a2abc25819ca3e5420ec10c2
gongyuhua <yg348@njit.edu>
2018-06-19 23:17:31 -0400

    group routes under a common middleware and handle API errors

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
-<?php
+?php
 
-Route::resource('/task', 'TaskController');
-Route::get('/category/{category}/tasks', 'CategoryController@tasks');
-Route::resource('/category', 'CategoryController');
 Route::post('login', 'UserController@login');
 Route::post('register', 'UserController@register');
+
+Route::group(['middleware' => 'auth:api'], function(){
+Route::resource('/task', 'TasksController');
+Route::resource('/category', 'CategoryController');
+Route::get('/category/{category}/tasks', 'CategoryController@tasks');
+});
